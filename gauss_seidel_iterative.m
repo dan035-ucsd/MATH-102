7c126a459d852347c0e70207ea35f5449961f4b4
Dani <dan035@ucsd.edu>
2025-10-17 14:49:14 -0700

    Math 174 Lecture 9

--- gauss_seidel_iterative.m
+++ gauss_seidel_iterative.m
@@ -5,23 +5,19 @@ b = [ 2 0 10 ];
 n = 3;
 N = 10;
 
-function [x] = gaussseidel(A, b, n, N)
-    for i = 1:n
-        x(i) = 0;       % set initial guess to zero vector
-    end
+function [x] = gaussseidel(A, b, n, N) 
+    x = zeros(n,1);     % set initial guess to zero vector
     for iterat = 1:N    % set number of iterations
         for row = 1:n   
-           sum1 = 0;     % reset first sum to zero for each row
-           sum2 = 0;     % reset second sum to zero for each row
-           for col = 1:row-1     
-                sum1 = sum1 + A(row,col) * x(col);  % how to iterate on x^(k+1)  
+           sum = 0;     % reset sum to zero for each row
+           for col = 1:n 
+                if row ~= col    
+                    sum = sum + A(row,col) * x(col);  
+                end
            end
-           for col = row+1:n
-               sum2 = sum2 + A(row,col) * x(col);
-           end
-           x(row) = ( b(row) - sum1 - sum2 ) / A(row,row)
+           x(row) = ( b(row) - sum ) / A(row,row)
         end
     end
 end
 
-x = gaussseidel(A,b,n,N);
+x = gaussseidel(A,b,n,N)
--- jacobi_iterative.m
+++ jacobi_iterative.m
@@ -3,23 +3,24 @@ A = [ 3  1  -1
       2 -2  5 ]; 
 b = [ 2 0 10 ];
 n = 3;
-N = 10;
+N = 3;
 
-function [x] = jacobi(A, b, n, N)
-    for i = 1:n
-        x(i) = 0;       % set initial guess to zero vector
-    end
+function [x] = jacobi(A, b, n, N) %need x old n 
+    x = zeros(n,1);     % set initial guess to zero vector
+    xold = zeros(n,1);  % create old x vector to store previous iteration values
     for iterat = 1:N    % set number of iterations
         for row = 1:n   
            sum = 0;     % reset sum to zero for each row
            for col = 1:n 
                 if row ~= col    
-                    sum = sum + A(row,col) * x(col);  
+                    sum = sum + A(row,col) * xold(col);  
                 end
            end
            x(row) = ( b(row) - sum ) / A(row,row)
         end
+        xold = x;
     end
 end
 
-x = jacobi(A,b,n,N);
+x = jacobi(A,b,n,N)
+r = b' - A * x  % compute the residual vector
