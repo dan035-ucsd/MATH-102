a33a7f719cc6c2686383a6b5c74203ac3b6dc5d4
dangorochimaru <dangorochimaru@gmail.com>
2025-10-23 21:12:30 -0700

    LU factorization

---
+++ MATLAB/lufact.m
@@ -0,0 +1,12 @@
+function [LU] = luFactorization(n, A)
+    LU = A;
+
+    for k = 1:n-1
+        for i = k+1:n
+            LU(i, k) = LU(i, k)/LU(k, k);
+            for j = k+1:n
+                LU(i, j) = LU(i, j) - (LU(i, k) * LU(k, j));
+            end
+        end
+    end
+end
