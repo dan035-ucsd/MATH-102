8a108730d9a6c5ca1d8d516f5d6375d1bb4cfb0c
dangorochimaru <dangorochimaru@gmail.com>
2025-11-07 16:03:35 -0800

    Lagrange Interp

---
+++ MATLAB/lagrange_interp.m
@@ -0,0 +1,29 @@
+function [val] = lagrangeinterp(x, y, n, z)
+    val = 0;
+    for i = 1:n
+        for j = 1:n
+            if j ~= i
+                val = val + y(i)*((z-x(j))/(x(i)-x(j)));
+            end
+        end
+    end
+end
+
+x = [0,100];
+y = [32, 212];
+n = length(x);
+z = 37;
+result = lagrangeinterp2(x, y, n, z)
+
+function [val] = lagrangeinterp2(x, y, n, z)
+    val = 0;
+    prod = 1;
+    for i = 1:n
+        for j = 1:n
+            if j ~= i
+                prod = prod * ((z-x(j))/(x(i)-x(j)));
+            end
+        end
+        val = val + y(i) * prod;
+    end
+end
